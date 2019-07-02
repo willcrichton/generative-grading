5278f7f22f758150844b16534b2bd00caaad5674
Georgia <gsamp@stanford.edu>
2019-07-02 14:40:09 -0700

    Will's input

--- src/rubricsampling/grammars/pythagorean/exponential.py
+++ src/rubricsampling/grammars/pythagorean/exponential.py
@@ -8,31 +8,55 @@ from base import Decision
 # Decision: exponential
 # ------------------------
 # Students may calculate exponentiation inline or use Math.pow()
+
 class exponential(Decision):
 	def registerChoices(self):
-		self.addChoice('usesMethod', {
-			True : 5,     # they calculate the exponential inline
-			False : 100   # they use Math.pow()
-		})
+		if not self.hasChoice('usesMethod'):
+			self.addChoice(f'usesMethod', {
+				True : 5,     # they calculate the exponential inline
+				False : 10  # they use Math.pow()
+			})
 
-	def renderCode(self):
-		if self.getChoice('usesMethod'):
-			return '{mathPow}'
+	def renderCode(self):		
+		if self.getChoice(f'usesMethod'):
+			return self.expand('mathPow', params=self.params)
 		else:
-			return '{mathInline}'
+			return self.expand('mathInline', params=self.params)
 
+# def make_exponential(name):
+# 	class exponential(Decision):
+# 		def registerChoices(self):
+# 			self.addChoice(f'{name}usesMethod', {
+# 				True : 5,     # they calculate the exponential inline
+# 				False : 10  # they use Math.pow()
+# 			})
+
+# 		def renderCode(self):
+# 			if self.getChoice(f'{name}usesMethod'):
+# 				return '{mathPow}'
+# 			else:
+# 				return '{mathInline}'
+# 	exponential.__name__ = name
+# 	return exponential
+
+# # memoization
+# fib_table = {}
+# def fib(n):
+# 	if n in fib_table:
+# 		return fib_table[n]
+# 	if n <= 1: return n
+# 	return fib(n-1) + fib(n-2)
+
+# exponential = make_exponential('exponential')
+# exponential2 = make_exponential('exponential2')
 
 # Decision: mathPow
 # ------------------------
 # Students use of Math.pow()
 class mathPow(Decision):
-	def renderCode(self):
-		aUsed = self.getState('aUsed')
-		if aUsed == False:
-			self.addOrSetState('aUsed', True)
-			return 'Math.pow(a,2)'
-		else:
-			return 'Math.pow(b,2)'
+	def renderCode(self):		
+		var = self.getState('var') #self.params['var']
+		return f'Math.pow({var},2)'
 
 
 # Decision: mathInline
@@ -40,11 +64,7 @@ class mathPow(Decision):
 # Students use of inline command '*'
 class mathInline(Decision):
 	def renderCode(self):
-		aUsed = self.getState('aUsed')
-		if aUsed == False:
-			self.addOrSetState('aUsed', True)
-			return 'a*a'
-		else:
-			return 'b*b'
+		var = self.getState('var') #self.params['var']
+		return f'{var}*{var}'
 
 
--- src/rubricsampling/grammars/pythagorean/solution.py
+++ src/rubricsampling/grammars/pythagorean/solution.py
@@ -6,22 +6,44 @@ import random
 from base import Decision
 
 class Solution(Decision):
+	def registerChoices(self):
+		self.addChoice('firstVar', {
+			'a': 1,
+			'x': 1
+		})
+		self.addChoice('secondVar', {
+			'b': 1,
+			'y': 1
+		})		
+
 	def renderCode(self):
 		self.addState('aUsed', False)
-		
+		self.addState('var', None)
+
+		firstVar = self.getChoice('firstVar')
+		secondVar = self.getChoice('secondVar')
+			
+		self.setState('var', firstVar)
+		exponential1 = self.expand('exponential')
+		self.setState('var', secondVar)
+		exponential2 = self.expand('exponential')
+
 		templateVars = {
 			'mainPrompt': self.expand('mainPrompt'),
 			'inputPrompt': self.expand('inputPrompt'),
-			'exponential': self.expand('exponential'),
-			'outputPrompt': self.expand('outputPrompt')
+			'exponential1': exponential1, #self.expand('exponential', params={'var': firstVar}),
+			'exponential2': exponential2, #self.expand('exponential', params={'var': secondVar}),
+			'outputPrompt': self.expand('outputPrompt'),
+			'firstVar': firstVar,
+			'secondVar': secondVar
 		}	
 
 		# assumes all students used doubles
 		template = '''
 		println("{mainPrompt}");
-		double a = readDouble("{inputPrompt}a:");
-		double b = readDouble("{inputPrompt}b:");
-		double c = Math.sqrt({exponential} + {exponential});
+		double {firstVar} = readDouble("{inputPrompt}a:");
+		double {secondVar} = readDouble("{inputPrompt}b:");
+		double c = Math.sqrt({exponential1} + {exponential2});
 		println("{outputPrompt}" + c);
 		'''
 
